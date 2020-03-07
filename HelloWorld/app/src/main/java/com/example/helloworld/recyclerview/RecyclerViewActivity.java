142061e2c2aecbe7705daf03e565a26dd28aae2c
cuikeyu <2587357943@qq.com>
2020-03-08 02:56:45 +0800

    RecyclerView(二)

--- HelloWorld/app/src/main/java/com/example/helloworld/recyclerview/RecyclerViewActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/recyclerview/RecyclerViewActivity.java
@@ -1,17 +1,17 @@
 package com.example.helloworld.recyclerview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.helloworld.R;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
-    private Button mBtnLinear;
+    private Button mBtnLinear, mBtnHor, mBtnGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +25,22 @@ public class RecyclerViewActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        mBtnHor = findViewById(R.id.btn_hor);
+        mBtnHor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecyclerViewActivity.this, HorRecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        mBtnGrid = findViewById(R.id.btn_grid);
+        mBtnGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecyclerViewActivity.this, GridRecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
