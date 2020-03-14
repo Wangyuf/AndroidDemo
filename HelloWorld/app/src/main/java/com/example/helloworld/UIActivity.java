538995d07384556bba6be7ed3f0398d996ce25f8
cuikeyu <2587357943@qq.com>
2020-03-14 12:54:58 +0800

    ProgressBar && ProgressDialog

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/UIActivity.java
@@ -0,0 +1,116 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.helloworld.gridview.GridViewActivity;
+import com.example.helloworld.listview.ListViewActivity;
+import com.example.helloworld.recyclerview.RecyclerViewActivity;
+
+public class UIActivity extends AppCompatActivity {
+
+    private Button mbtnTextView;
+    private Button mBtnButton;
+    private Button mBtnEditText;
+    private Button mRadioButton;
+    private Button mCheckBox;
+    private Button mBtnImgaeView;
+    private Button mBtnListView;
+    private Button mBtnGridView;
+    private Button mBtnRecyclerView;
+    private Button mBtnWebView;
+    private Button mBtnToast;
+    private Button mDialog;
+    private Button mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui);
+        mbtnTextView = findViewById(R.id.btn_textview);
+        mBtnButton = findViewById(R.id.btn_button);
+        mBtnEditText = findViewById(R.id.btn_edittext);
+        mRadioButton = findViewById(R.id.btn_radiobutton);
+        mCheckBox = findViewById(R.id.btn_checkbox);
+        mBtnImgaeView = findViewById(R.id.btn_imageview);
+        mBtnListView = findViewById(R.id.btn_listview);
+        mBtnGridView = findViewById(R.id.btn_gridview);
+        mBtnRecyclerView = findViewById(R.id.btn_recyclerview);
+        mBtnWebView = findViewById(R.id.btn_webview);
+        mBtnToast = findViewById(R.id.btn_toast);
+        mDialog = findViewById(R.id.btn_dialog);
+        mProgress = findViewById(R.id.btn_progress);
+        setListeners();
+    }
+
+    private void setListeners(){
+        Onclick onclick = new Onclick();
+        mbtnTextView.setOnClickListener(onclick);
+        mBtnButton.setOnClickListener(onclick);
+        mBtnEditText.setOnClickListener(onclick);
+        mRadioButton.setOnClickListener(onclick);
+        mCheckBox.setOnClickListener(onclick);
+        mBtnImgaeView.setOnClickListener(onclick);
+        mBtnListView.setOnClickListener(onclick);
+        mBtnGridView.setOnClickListener(onclick);
+        mBtnRecyclerView.setOnClickListener(onclick);
+        mBtnWebView.setOnClickListener(onclick);
+        mBtnToast.setOnClickListener(onclick);
+        mDialog.setOnClickListener(onclick);
+        mProgress.setOnClickListener(onclick);
+    }
+
+    private class Onclick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            switch (v.getId()) {
+                case R.id.btn_textview:
+                    intent = new Intent(UIActivity.this, EditTextActivity.class);
+                    break;
+                case R.id.btn_button:
+                    intent = new Intent(UIActivity.this, ButtonActivity.class);
+                    break;
+                case R.id.btn_edittext:
+                    intent = new Intent(UIActivity.this, EditTextActivity.class);
+                    break;
+                case R.id.btn_radiobutton:
+                    intent = new Intent(UIActivity.this, RadioButtonActivity.class);
+                    break;
+                case R.id.btn_checkbox:
+                    intent = new Intent(UIActivity.this, CheckBoxActivity.class);
+                    break;
+                case R.id.btn_imageview:
+                    intent = new Intent(UIActivity.this, ImageViewActivity.class);
+                    break;
+                case R.id.btn_listview:
+                    intent = new Intent(UIActivity.this, ListViewActivity.class);
+                    break;
+                case R.id.btn_gridview:
+                    intent = new Intent(UIActivity.this, GridViewActivity.class);
+                    break;
+                case R.id.btn_recyclerview:
+                    intent = new Intent(UIActivity.this, RecyclerViewActivity.class);
+                    break;
+                case R.id.btn_webview:
+                    intent = new Intent(UIActivity.this, WebViewActivity.class);
+                    break;
+                case R.id.btn_toast:
+                    intent = new Intent(UIActivity.this, ToastActivity.class);
+                    break;
+                case R.id.btn_dialog:
+                    intent = new Intent(UIActivity.this, DialogActivity.class);
+                    break;
+                case R.id.btn_progress:
+                    intent = new Intent(UIActivity.this, ProgressActivity.class);
+                    break;
+            }
+            startActivity(intent);
+
+        }
+    }
+}
