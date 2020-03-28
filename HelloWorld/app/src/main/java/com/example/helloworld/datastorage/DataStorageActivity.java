b5897783522cdad4109a749c9db3f24deef8afc9
cuikeyu <2587357943@qq.com>
2020-03-28 21:57:43 +0800

    BroadCastReceiver

--- HelloWorld/app/src/main/java/com/example/helloworld/datastorage/DataStorageActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/datastorage/DataStorageActivity.java
@@ -2,16 +2,18 @@ package com.example.helloworld.datastorage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.helloworld.R;
+import com.example.helloworld.broadcast.BroadActivity;
 
 public class DataStorageActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mBtnSharedPreferences,mBtnFile,mBtnFileMount;
+    private Button mBtnSharedPreferences,mBtnFile,mBtnFileMount,mBtnBroad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +21,11 @@ public class DataStorageActivity extends AppCompatActivity implements View.OnCli
         mBtnSharedPreferences = findViewById(R.id.btn_sharedpreferences);
         mBtnFile = findViewById(R.id.btn_file);
         mBtnFileMount = findViewById(R.id.btn_file_mount);
+        mBtnBroad = findViewById(R.id.btn_broad);
         mBtnSharedPreferences.setOnClickListener(DataStorageActivity.this);
         mBtnFile.setOnClickListener(DataStorageActivity.this);
         mBtnFileMount.setOnClickListener(DataStorageActivity.this);
+        mBtnBroad.setOnClickListener(DataStorageActivity.this);
     }
 
     @Override
@@ -36,6 +40,10 @@ public class DataStorageActivity extends AppCompatActivity implements View.OnCli
             case R.id.btn_file_mount:
                 intent = new Intent(DataStorageActivity.this, FileMountActivity.class);
                 break;
+            case R.id.btn_broad:
+                intent = new Intent(DataStorageActivity.this, BroadActivity.class);
+                break;
+
         }
         startActivity(intent);
     }
